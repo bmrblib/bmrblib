2147cf6dd7f127d44c513edfc14793f6efe2e017
Edward d'Auvergne <edward@nmr-relax.com>
2011-02-02 15:31:22 +0000

    Added the v2.1 sample_conditions saveframe.

--- bmrblib/nmr_star_dict_v2_1.py
+++ bmrblib/nmr_star_dict_v2_1.py
@@ -33,7 +33,7 @@ from bmrblib.citations.citations import CitationsSaveframe
 from bmrblib.experimental_details.experiment import ExperimentSaveframe
 from bmrblib.experimental_details.method import MethodSaveframe
 from bmrblib.experimental_details.nmr_spectrometer import NMRSpectrometerSaveframe
-from bmrblib.experimental_details.sample_conditions import SampleConditionsSaveframe
+from bmrblib.experimental_details.sample_conditions_v2_1 import SampleConditionsSaveframe_v2_1
 from bmrblib.experimental_details.software import SoftwareSaveframe
 from bmrblib.kinetics.relaxation import Relaxation
 from bmrblib.NMR_parameters.chem_shift_anisotropy import ChemShiftAnisotropySaveframe
@@ -77,7 +77,7 @@ class NMR_STAR_v2_1:
         self.experiment = ExperimentSaveframe(self.data.datanodes)
         self.method = MethodSaveframe(self.data.datanodes)
         self.nmr_spectrometer = NMRSpectrometerSaveframe(self.data.datanodes)
-        self.sample_conditions = SampleConditionsSaveframe(self.data.datanodes)
+        self.sample_conditions = SampleConditionsSaveframe_v2_1(self.data.datanodes)
         self.software = SoftwareSaveframe(self.data.datanodes)
 
         # Initialise Supergroup 5:  The NMR parameters saveframe API.
