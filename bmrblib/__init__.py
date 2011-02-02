db6313d276609bd9e165991d98d8ab8b783d5925
Edward d'Auvergne <edward@nmr-relax.com>
2011-02-02 12:19:32 +0000

    Created the package function create_nmr_star() for initialising the correct version of the library.
    This returns the NMR-STAR python object.

--- bmrblib/__init__.py
+++ bmrblib/__init__.py
@@ -22,9 +22,38 @@
 # Module docstring.
 """Package for interfacing with the BMRB (http://www.bmrb.wisc.edu/) by handling NMR-STAR formatted files."""
 
-
+# The list of all modules and packages.
 __all__ = ['base_classes',
            'misc',
            'nmr_star_dict',
            'nmr_star_dict_v2_1',
            'nmr_star_dict_v3_1']
+
+# Module imports.
+from nmr_star_dict_v2_1 import NMR_STAR_v2_1
+from nmr_star_dict_v3_1 import NMR_STAR_v3_1
+
+
+def create_nmr_star(title, file_path, version=None):
+    """Initialise the NMR-STAR object.
+
+    @param title:       The title of the NMR-STAR data.
+    @type title:        str
+    @param file_path:   The full file path.
+    @type file_path:    str
+    @keyword version:   The NMR-STAR version to use.
+    @type version:      str
+    @return:            The NMR-STAR python object.
+    @rtype:             class instance
+    """
+
+    # Initialise the NMR-STAR data object.
+    if version == '3.1':
+        star = NMR_STAR_v3_1('relax_model_free_results', file_path)
+    elif version == '2.1':
+        star = NMR_STAR_v2_1('relax_model_free_results', file_path)
+    else:
+        raise NameError("The NMR-STAR version %s is unknown." % version)
+
+    # Return the object.
+    return star
