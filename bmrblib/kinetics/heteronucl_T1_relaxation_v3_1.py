266d9534e7db408dd3c2adc88a0be967dba9038b
Edward d'Auvergne <edward@nmr-relax.com>
2011-02-14 17:23:39 +0000

    Expanded the heteronuclear T1 v3.1 module to include all tag names.

--- bmrblib/kinetics/heteronucl_T1_relaxation_v3_1.py
+++ bmrblib/kinetics/heteronucl_T1_relaxation_v3_1.py
@@ -2,7 +2,7 @@
 #                                                                           #
 # The BMRB library.                                                         #
 #                                                                           #
-# Copyright (C) 2009-2010 Edward d'Auvergne                                 #
+# Copyright (C) 2009-2011 Edward d'Auvergne                                 #
 #                                                                           #
 # This program is free software: you can redistribute it and/or modify      #
 # it under the terms of the GNU General Public License as published by      #
@@ -63,10 +63,18 @@ class HeteronuclT1List_v3_1(HeteronuclT1List):
         self.tag_category_label = 'Heteronucl_T1_list'
 
         # Change tag names.
-        self['SfCategory'].tag_name = 'Sf_category'
-        self['SfFramecode'].tag_name = 'Sf_framecode'
+        self['SfCategory'].tag_name =               'Sf_category'
+        self['SfFramecode'].tag_name =              'Sf_framecode'
         self['HeteronuclT1ListID'].tag_name =       'ID'
+        self['DataFileName'].tag_name =             'Data_file_name'
+        self['SampleConditionListID'].tag_name =    'Sample_condition_list_ID'
         self['SampleConditionListLabel'].tag_name = 'Sample_condition_list_label'
+        self['SpectrometerFrequency1H'].tag_name =  'Spectrometer_frequency_1H'
+        self['T1CoherenceType'].tag_name =          'T1_coherence_type'
+        self['T1ValUnits'].tag_name =               'T1_val_units'
+        self['Details'].tag_name =                  'Details'
+        self['TextDataFormat'].tag_name =           'Text_data_format'
+        self['TextData'].tag_name =                 'Text_data'
 
 
 
@@ -86,6 +94,16 @@ class HeteronuclT1Experiment_v3_1(HeteronuclT1Experiment):
         # The category name.
         self.tag_category_label = 'Heteronucl_T1_experiment'
 
+        # Change tag names.
+        self['ExperimentID'].tag_name =         'Experiment_ID'
+        self['ExperimentName'].tag_name =       'Experiment_name'
+        self['SampleID'].tag_name =             'Sample_ID'
+        self['SampleLabel'].tag_name =          'Sample_label'
+        self['SampleState'].tag_name =          'Sample_state'
+        self['EntryID'].tag_name =              'Entry_ID'
+        self['HeteronuclT1ListID'].tag_name =   'Heteronucl_T1_list_ID'
+
+
 
 class HeteronuclT1Software_v3_1(HeteronuclT1Software):
     """v3.1 HeteronuclT1Software tag category."""
@@ -102,6 +120,14 @@ class HeteronuclT1Software_v3_1(HeteronuclT1Software):
 
         # The category name.
         self.tag_category_label = 'Heteronucl_T1_software'
+
+        # Change tag names.
+        self['SoftwareID'].tag_name =           'Software_ID'
+        self['SoftwareLabel'].tag_name =        'Software_label'
+        self['MethodID'].tag_name =             'Method_ID'
+        self['MethodLabel'].tag_name =          'Method_label'
+        self['EntryID'].tag_name =              'Entry_ID'
+        self['HeteronuclT1ListID'].tag_name =   'Heteronucl_T1_list_ID'
 
 
 
@@ -122,9 +148,22 @@ class T1_v3_1(T1):
         self.tag_category_label = 'T1'
 
         # Change tag names.
-        self['RxID'].tag_name =         'ID'
-        self['CompIndexID'].tag_name =  'Comp_index_ID'
-        self['CompID'].tag_name =       'Comp_ID'
-        self['AtomID'].tag_name =       'Atom_ID'
-        self['Val'].tag_name =          'Val'
-        self['ValErr'].tag_name =       'Val_err'
+        self['RxID'].tag_name =                 'ID'
+        self['AssemblyAtomID'].tag_name =       'Assembly_atom_ID'
+        self['EntityAssemblyID'].tag_name =     'Entity_assembly_ID'
+        self['EntityID'].tag_name =             'Entity_ID'
+        self['CompIndexID'].tag_name =          'Comp_index_ID'
+        self['SeqID'].tag_name =                'Seq_ID'
+        self['CompID'].tag_name =               'Comp_ID'
+        self['AtomID'].tag_name =               'Atom_ID'
+        self['AtomType'].tag_name =             'Atom_type'
+        self['AtomIsotopeNumber'].tag_name =    'Atom_isotope_number'
+        self['Val'].tag_name =                  'Val'
+        self['ValErr'].tag_name =               'Val_err'
+        self['ResonanceID'].tag_name =          'Resonance_ID'
+        self['AuthEntityAssemblyID'].tag_name = 'Auth_entity_assembly_ID'
+        self['AuthSeqID'].tag_name =            'Auth_seq_ID'
+        self['AuthCompID'].tag_name =           'Auth_comp_ID'
+        self['AuthAtomID'].tag_name =           'Auth_atom_ID'
+        self['EntryID'].tag_name =              'Entry_ID'
+        self['HeteronuclT1ListID'].tag_name =   'Heteronucl_T1_list_ID'
