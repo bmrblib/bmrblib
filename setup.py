70bc88e61b266e7f96435ae674df4a64b922a25e
Edward d'Auvergne <edward@nmr-relax.com>
2024-06-04 12:32:45 +0200

    Distribution: Switching to setuptools to fix the depreciation of distutils.
    From Python 3.12, the distutils package is no longer supported.

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 
-# Setup for distutils.
-setup(name='bmrblib',
-      version='trunk',
-      description='The BMRB library',
-      author="Edward d'Auvergne",
-      author_email='edward@nmr-relax.com',
-      url='https://gna.org/projects/bmrblib/',
-      license='GPL',
-      packages=['bmrblib',
-                'bmrblib.assembly_supercategory',
-                'bmrblib.citations',
-                'bmrblib.entry_information',
-                'bmrblib.experimental_details',
-                'bmrblib.kinetics',
-                'bmrblib.NMR_parameters',
-                'bmrblib.pystarlib',
-                'bmrblib.structure',
-                'bmrblib.thermodynamics'
-      ]
-)
+setup(version='master')
